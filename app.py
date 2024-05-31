c937d7911f54351b4fb984b09ec9bcf5fa7ee0b3
Prakhar2295 <prakhar.kumar5@gmail.com>
2024-05-31 09:11:43 +0530

    opentelemetry code updated

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
+import os
 from flask import Flask
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
 
 app = Flask(__name__)
 
+# Set up OpenTelemetry
+trace.set_tracer_provider(TracerProvider())
+tracer = trace.get_tracer(__name__)
+
+# Read the OTLP endpoint from the environment variable
+otlp_endpoint = os.getenv('OTLP_ENDPOINT', 'http://localhost:4317')
+
+# Configure the OTLP exporter
+otlp_exporter = OTLPSpanExporter(endpoint=otlp_endpoint, insecure=True)
+
+# Create a BatchSpanProcessor and add it to the tracer provider
+span_processor = BatchSpanProcessor(otlp_exporter)
+trace.get_tracer_provider().add_span_processor(span_processor)
+
+# Instrument Flask with OpenTelemetry
+FlaskInstrumentor().instrument_app(app)
+
 @app.route('/hello')
 def hello_world():
-    return 'Hello, World!'
+    with tracer.start_as_current_span("hello_world_span"):
+        return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True,port=5000)
